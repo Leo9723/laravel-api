cd5f502e8bc1ea5f9750ccec9df0f62ee6b16255
Leo9723 <pala9761@gmail.com>
2023-03-13 15:42:53 +0100

    aggiunta controller e model per la form contacts

---
+++ resources/views/emails/guest_email.blade.php
@@ -0,0 +1,5 @@
+<h5>name: {{ $lead->name }}</h5>
+<h5>surname: {{ $lead->surname }}</h5>
+<h5>mail: {{ $lead->mail }}</h5>
+<h5>phone: {{ $lead->phone }}</h5>
+<h5>message: {{ $lead->message }}</h5>
